a95147aa50142b9f5f68888bc08bade5aa915b79
SephorahM <st20316678@outlook.cardiffmet.ac.uk>
2025-05-07 20:51:35 +0100

    some adjustments

--- userhomepage.php
+++ userhomepage.php
@@ -33,8 +33,10 @@ session_start();
     <div id="user home" class="container active" style="background: teal;">
     <h1>Welcome, <?php echo $user_data['username']; ?>!</h1>
     <div class="carousel-inner" style="display: flex; transition: transform 0.5s ease; width: 100%;">
-                <img src="https://www.redocean.co.uk/image/cache/products/27852/image03_2000-1500x1500.jpg" style="flex: 0 0 100%; height: 50vw; max-height: 400px; object-fit: cover;">
-            </div>
-    <h2>Let's get you started with facilitating users with yours cravans</h2>
+        <img src="https://www.redocean.co.uk/image/cache/products/27852/image03_2000-1500x1500.jpg" style="flex: 0 0 100%; height: 50vw; max-height: 400px; object-fit: cover;">
+    </div>
+    <h2>Let's get you started with renting your caravans</h2>
+    <button onclick="window.location.href='addcaravan.php'" style="margin: 20px 0; padding: 10px 20px; background: orange; color: white; border: none; cursor: pointer;">Add a Caravan</button>
+
 </body>
 </html>
